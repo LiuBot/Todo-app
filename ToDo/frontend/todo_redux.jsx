d66e223a5c17c788747c23d1485181b1ff5318b6
Sophia Liu <sliu3@wellesley.edu>
2016-11-20 19:19:48 -0800

    phase 2

---
+++ ToDo/frontend/todo_redux.jsx
@@ -0,0 +1,13 @@
+ // This is the entry doc
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", () =>{ // add event listener, pass it a callback 
+	const rootEl = document.getElementById('content');
+
+	ReactDOM.render(rootEl)
+})
+
+
+ <h1>Todos App</h1>
