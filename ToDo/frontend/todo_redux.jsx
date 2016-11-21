6836837550fafad776a1d2481c9ffd315391f00f
Sophia Liu <sliu3@wellesley.edu>
2016-11-21 09:13:18 -0800

    State shape

--- ToDo/frontend/todo_redux.jsx
+++ ToDo/frontend/todo_redux.jsx
@@ -2,12 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {fetchTodos} from './util/todo_api_util';
+
 
 document.addEventListener("DOMContentLoaded", () =>{ // add event listener, pass it a callback 
-	const rootEl = document.getElementById('content');
-
-	ReactDOM.render(rootEl)
+	const root = document.getElementById('content');
+	window.fetchTodos = fetchTodos;
+	ReactDOM.render(root);
 })
 
-
- <h1>Todos App</h1>
---
+++ ToDo/frontend/util/todo_api_util.js
@@ -0,0 +1,9 @@
+
+export const fetchTodos = (success, error) => {
+  $.ajax({
+    method: 'GET',
+    url: 'api/todos',
+    success,
+    error
+  });
+};
